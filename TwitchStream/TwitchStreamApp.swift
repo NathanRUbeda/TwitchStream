9658d638816031928f5dc56b0197ad7c16e89c65
nathanrubeda <ubedanathan@gmail.com>
2024-10-16 18:39:40 -0400

    Initial Commit

---
+++ TwitchStream/TwitchStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitchStreamApp.swift
+//  TwitchStream
+//
+//  Created by Nathan Ubeda on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct TwitchStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
